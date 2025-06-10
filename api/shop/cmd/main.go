3802cd2a7e78c0e6b6986195c905562632c847fd
tamaco489 <tamaco489@gmail.com>
2025-06-11 05:55:36 +0900

    add: health check api

--- api/shop/cmd/main.go
+++ api/shop/cmd/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+)
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"status":"OK"}`))
+}
 
 func main() {
-	fmt.Println("start shop api server...")
+	ctx := context.Background()
+
+	http.HandleFunc("/api/healthcheck", healthCheckHandler)
+
+	slog.InfoContext(ctx, "Server starting on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
