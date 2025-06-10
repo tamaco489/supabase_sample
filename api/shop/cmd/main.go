a97efef9836255188cfb9d091bb96380359c465d
tamaco489 <tamaco489@gmail.com>
2025-06-11 06:46:59 +0900

    fix: use db connection for get me api usecase

--- api/shop/cmd/main.go
+++ api/shop/cmd/main.go
@@ -7,13 +7,15 @@ import (
 	"net/http"
 
 	"github.com/tamaco489/supabase_sample/api/shop/intrnal/handler"
+	"github.com/tamaco489/supabase_sample/api/shop/intrnal/repository"
 )
 
 func main() {
 	ctx := context.Background()
+	repository.InitDB(ctx)
 
 	http.HandleFunc("/shop/v1/healthcheck", handler.HealthCheckHandler)
-	http.HandleFunc("/shop/v1/users/me", handler.GetMeMock)
+	http.HandleFunc("/shop/v1/users/me", handler.GetMe)
 
 	slog.InfoContext(ctx, "Server starting on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
--- api/shop/intrnal/handler/user.go
+++ api/shop/intrnal/handler/user.go
@@ -5,29 +5,53 @@ import (
 	"log/slog"
 	"net/http"
 	"time"
+
+	"github.com/tamaco489/supabase_sample/api/shop/intrnal/repository"
 )
 
 type User struct {
-	ID          json.RawMessage `json:"id"`
-	UserName    string          `json:"username"`
-	Email       string          `json:"email"`
-	Role        string          `json:"role"`
-	Status      string          `json:"status"`
-	LastLoginAt time.Time       `json:"last_login_at"`
-	CreatedAt   time.Time       `json:"created_at"`
-	UpdatedAt   time.Time       `json:"updated_at"`
+	ID          string    `json:"id"`
+	UserName    string    `json:"username"`
+	Email       string    `json:"email"`
+	Role        string    `json:"role"`
+	Status      string    `json:"status"`
+	LastLoginAt time.Time `json:"last_login_at"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
-func GetMeMock(w http.ResponseWriter, r *http.Request) {
-	user := User{
-		ID:          json.RawMessage(`"0cefb56b-55c7-41ba-97c7-c30ee4918dc2"`),
-		UserName:    "tamaco489",
-		Email:       "tamaco489@gmail.com",
-		Role:        "user",
-		Status:      "active",
-		LastLoginAt: time.Date(2025, 6, 11, 0, 0, 0, 0, time.UTC),
-		CreatedAt:   time.Date(2025, 6, 11, 0, 0, 0, 0, time.UTC),
-		UpdatedAt:   time.Date(2025, 6, 11, 0, 0, 0, 0, time.UTC),
+func GetMe(w http.ResponseWriter, r *http.Request) {
+
+	db := repository.GetPool()
+
+	// NOTE: 本来はJWT等から取得するべき
+	uid := "0cefb56b-55c7-41ba-97c7-c30ee4918dc2"
+
+	rows, err := db.Query(r.Context(), "SELECT * FROM users WHERE id = $1", uid)
+	if err != nil {
+		slog.ErrorContext(r.Context(), "failed to query users", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	defer rows.Close()
+
+	user := User{}
+	for rows.Next() {
+		if err := rows.Scan(
+			&user.ID,
+			&user.UserName,
+			&user.Email,
+			&user.Role,
+			&user.Status,
+			&user.LastLoginAt,
+			&user.CreatedAt,
+			&user.UpdatedAt,
+		); err != nil {
+			slog.ErrorContext(r.Context(), "failed to scan user", "error", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 	}
 
 	w.Header().Set("Content-Type", "application/json")
