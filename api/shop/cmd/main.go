48a7c005c0e852595916cb5e6cfc4130046d3df8
tamaco489 <tamaco489@gmail.com>
2025-06-11 06:10:42 +0900

    add: get me api

--- api/shop/cmd/main.go
+++ api/shop/cmd/main.go
@@ -12,7 +12,8 @@ import (
 func main() {
 	ctx := context.Background()
 
-	http.HandleFunc("/api/healthcheck", handler.HealthCheckHandler)
+	http.HandleFunc("/shop/v1/healthcheck", handler.HealthCheckHandler)
+	http.HandleFunc("/shop/v1/users/me", handler.GetMeMock)
 
 	slog.InfoContext(ctx, "Server starting on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
