6a6aacbfc177339167b47307d6d0be69e418cd60
tamaco489 <tamaco489@gmail.com>
2025-06-11 06:57:36 +0900

    fix: db config

--- api/shop/intrnal/repository/connect.go
+++ api/shop/intrnal/repository/connect.go
@@ -2,8 +2,10 @@ package repository
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"sync"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -14,18 +16,43 @@ var (
 	once   sync.Once
 )
 
-const dsn = ""
+type DBConfig struct {
+	User     string
+	Password string
+	Host     string
+	Port     string
+	DBName   string
+	SSLMode  string
+}
+
+func GetDBConfig() DBConfig {
+	return DBConfig{
+		User:     "",
+		Password: "",
+		Host:     "",
+		Port:     "",
+		DBName:   "",
+		SSLMode:  "verify-full",
+	}
+}
+
+func getDSN(config DBConfig) string {
+	return fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=%s", config.User, config.Password, config.Host, config.Port, config.DBName, config.SSLMode)
+}
 
 func InitDB(ctx context.Context) {
 	once.Do(func() {
-		config, err := pgxpool.ParseConfig(dsn)
+		cfg, err := pgxpool.ParseConfig(getDSN(GetDBConfig()))
 		if err != nil {
 			log.Fatalf("Failed to parse DB config: %v", err)
 		}
 
-		config.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeSimpleProtocol
+		// NOTE: デフォルトでは cache_statement になっているので、SimpleProtocol に変更
+		cfg.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeSimpleProtocol
+		cfg.MaxConns = 2
+		cfg.MaxConnLifetime = 1 * time.Minute
 
-		dbPool, err = pgxpool.NewWithConfig(ctx, config)
+		dbPool, err = pgxpool.NewWithConfig(ctx, cfg)
 		if err != nil {
 			log.Fatalf("Failed to connect to database: %v", err)
 		}
